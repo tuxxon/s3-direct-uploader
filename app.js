65c38885a691efefd7a42806515a2f529e45d2da
Gordon Ahn <tuxxon@nate.com>
2020-05-11 08:31:54 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var uploader = new qq.s3.FineUploader({
+    debug: false, // defaults to false
+    element: document.getElementById('fine-uploader'),
+    request: {
+        // S3 Bucket URL
+        endpoint: 'https://cf.stackcraft.co.s3.amazonaws.com', 
+        // iam ACCESS KEY
+        accessKey: 'AKIAXTQ5R7IXDS464SUP' 
+    },
+    objectProperties: {
+        region: 'ap-northeast-2',
+        key(fileId) {
+            var prefixPath = 'uploads'
+            var filename = this.getName(fileId)
+            return prefixPath + '/' + filename
+        }
+    },
+    signature: {
+        // version
+        version: 4,
+        // AWS API Gate URL
+        endpoint: 'https://ie4lf9ccwb.execute-api.ap-northeast-2.amazonaws.com/prod'
+    },
+    retry: {
+        enableAuto: true // defaults to false
+    }
+});
